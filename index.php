2c8858bd85eb967eabbc2703291b9a84b3c3fc58
novalhabibi <34689831+novalhabibi@users.noreply.github.com>
2019-08-20 15:56:08 +0700

    Add modal Logout

--- index.php
+++ index.php
@@ -33,10 +33,28 @@
                     <a class="nav-link" href="#">Link</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#" >Something</a>
                 </li>
+                
             </ul>
         </div>
+         <!--Dropdown primary-->
+        <div class="dropdown">
+
+            <!--Trigger-->
+            <a class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Material dropdown</a>
+
+
+            <!--Menu-->
+            <div class="dropdown-menu dropdown-primary">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logout">Logout</a>
+            </div>
+        </div>
+        <!--/Dropdown primary-->
     </nav>
 
     <div class="container">
@@ -157,10 +175,8 @@
             </div>
             <div class="col-md-9">
                 <figure class="col-md-4">
-                    <a href="./img(117).jpg" class=" mdb-lightbox"
-                        data-size="1600x1067">
-                        <img src="./img(117).jpg"
-                            class="img-fluid">
+                    <a href="./img(117).jpg" class=" mdb-lightbox" data-size="1600x1067">
+                        <img src="./img(117).jpg" class="img-fluid">
                     </a>
                 </figure>
                 <div class="row">
@@ -238,7 +254,60 @@
     </div>
 
 
+    <!-- Modal -->
+    <!-- Logout -->
+    <div class="modal fade" id="logout" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-notify modal-warning" role="document">
+            <!--Content-->
+            <div class="modal-content">
+                <!--Header-->
+                <div class="modal-header">
+                    <p class="heading">Modal Warning</p>
 
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="white-text">&times;</span>
+                    </button>
+                </div>
+
+                <!--Body-->
+                <div class="modal-body">
+
+                    <div class="row">
+                        <div class="col-3 text-center">
+                            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg"
+                                alt="Michal Szymanski - founder of Material Design for Bootstrap"
+                                class="img-fluid z-depth-1-half rounded-circle">
+                            <div style="height: 10px"></div>
+                            <p class="title mb-0">Jane</p>
+                            <p class="text-muted " style="font-size: 13px">Consultant</p>
+                        </div>
+
+                        <div class="col-9">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, molestiae provident
+                                temporibus
+                                sunt earum.</p>
+                            <p class="card-text">
+                                <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong>
+                            </p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <!--Footer-->
+                <div class="modal-footer justify-content-center">
+                    <a type="button" class="btn btn-warning">Get it now
+                        <i class="far fa-gem ml-1 text-white"></i>
+                    </a>
+                    <a type="button" class="btn btn-outline-warning waves-effect" data-dismiss="modal">No, thanks</a>
+                </div>
+            </div>
+            <!--/.Content-->
+        </div>
+    </div>
+    <!-- End Logout -->
 
     <!-- JQuery -->
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
